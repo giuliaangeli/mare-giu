1196eae7ad4b6eddb39469d3262acf3fc6b69388
Maria Regina <mreginaluzsilva@gmail.com>
2026-03-01 16:08:32 -0300

    feat: add app_name namespace to polls/urls.py

--- polls/urls.py
+++ polls/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = "polls"
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("mare", views.marelinda, name="marelinda"),
-    path('detail/<int:question_id>/', views.detail, name = 'detail'),
-    path('results/<int:question_id>/', views.results, name = 'results'),
-    path('vote/<int:question_id>/', views.vote, name = 'vote'),
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
