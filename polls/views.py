eec5ecff446008bd13cad44e63cb08878f987d4a
Maria Regina <mreginaluzsilva@gmail.com>
2026-03-13 16:33:13 -0300

    feat: filter out questions without choices

--- polls/views.py
+++ polls/views.py
@@ -15,7 +15,7 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         """Return the last five published questions (not including those set to be
         published in the future)."""
-        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now(), choice__isnull=False).distinct().order_by("-pub_date")[:5]
 
 
 class DetailView(generic.DetailView):
@@ -26,7 +26,7 @@ class DetailView(generic.DetailView):
         """
         Excludes any questions that aren't published yet.
         """
-        return Question.objects.filter(pub_date__lte=timezone.now())
+        return Question.objects.filter(pub_date__lte=timezone.now(), choice__isnull=False)
 
 
 class ResultsView(generic.DetailView):
@@ -37,7 +37,7 @@ class ResultsView(generic.DetailView):
         """
         Excludes any questions that aren't published yet.
         """
-        return Question.objects.filter(pub_date__lte=timezone.now())
+        return Question.objects.filter(pub_date__lte=timezone.now(), choice__isnull=False)
 
 
 def vote(request, question_id):
